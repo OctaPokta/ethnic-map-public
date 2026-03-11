78dbd0041653c704cc8148f94dc6215098ea5f0f
OctaPokta <reuvenigal100@gmail.com>
2026-03-11 02:46:35 +0200

    akpha 0.0.5 - Updated welcome description and added release notes link

--- js/data-config.js
+++ js/data-config.js
@@ -25,8 +25,8 @@ window.DashboardData = {
     poweredByBrand: "The Security Front",
     showUnderDevelopment: true,
     underDevelopmentText: "🚧 תחת פיתוח - גרסת אלפא 🚧",
-    versionText: "Alpha 0.0.4", 
-    releaseNotesLink: "https://github.com/OctaPokta/ethnic-map-public/releases/tag/v0.0.4-Alpha" // 🔥 NEW: Link to release notes
+    versionText: "Alpha 0.0.5", 
+    releaseNotesLink: "https://github.com/OctaPokta/ethnic-map-public/releases/tag/v0.0.5-Alpha" // 🔥 NEW: Link to release notes
   },
 
   // --- 2. BASE IMAGE PATHS ---
